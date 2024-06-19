f94f26f26e1e3dae733e62840f74eefe06b15588
Arioren <arioren100@gmail.com>
2024-06-19 15:04:21 +0300

    Update indexS.js
    סופי רביעי

--- indexS.js
+++ indexS.js
@@ -21,35 +21,36 @@ app.get('/api/users/:userName/:password', async(req, res) => {
 
 app.get('/api/therapists', async(req, res) => {
         const user = await db.getTHerapists()
-        res.status(500).send(user);
+        res.status(200).send(user);
 });
 
 app.get('/api/therapists/area/:region', async(req, res) => {
     console.log("indexjs work");
     const user = await db.getTHerapists("location",req.params.region)
-    res.status(500).send(user);
+    res.status(200).send(user);
 });
 
 app.get('/api/therapists/specialty/:specialty', async(req, res) => {
     const user = await db.getTHerapistsBySpecialty(req.params.specialty)
-    res.status(500).send(user);
+    res.status(200).send(user);
 });
 
 
-app.get('/api/appointments/:therapistsId', async(req, res) => {
+app.get('/api/appointments/:therapistsId', async(req, res) => {  
     const user = await db.getAppointmentsById(req.params.therapistsId)
-    res.status(500).send(user);
+    res.status(200).send(user);
 });
 
 app.post('/api/appointments/:therapistsId/:objectId', async(req, res) => {
     const userId = req.body
-    const user = await db.postNewAppointment(req.params.therapistsId   ,req.params.objectId    ,userId)
-    res.status(500).send(user);
+    console.log(userId);
+    const user = await db.postNewAppointment(req.params.therapistsId   ,req.params.objectId    ,userId.userId)
+    res.status(200).send(user);
 });
 
-app.get('/api/appointments/:userId', async(req, res) => {
+app.get('/api/appointments/all/:userId', async(req, res) => {
     const user = await db.getUserAppointments(req.params.userId)
-    res.status(500).send(user);
+    res.status(200).send(user);
 });
 
 const port = 3001;
